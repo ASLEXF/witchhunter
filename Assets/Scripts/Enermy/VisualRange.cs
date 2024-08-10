ba189ee68cf74e8a192e87620a6577d899d438c5
ASLEXF <getsheng6456@gmail.com>
2024-08-10 22:46:35 +0800

    update: shadow sprites, scene cliff

--- Assets/Scripts/Enermy/VisualRange.cs
+++ Assets/Scripts/Enermy/VisualRange.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class VisualRange : MonoBehaviour
 {
     [SerializeField] public float distance = 12.0f;
-    [SerializeField][Range(2, 12)] public int lineNum = 6;
+    [SerializeField][Range(2, 12)] public int lineNum = 2;
     [SerializeField][Range(90, 180)] public float range = 72.0f;
 
     Vector2 basePosition;
     Vector2 facePosition;  // rotate
 
     List<LineRenderer> lineRenderers = new List<LineRenderer>();
+    List<LineRenderer> edges = new List<LineRenderer>();
 
     private void Update()
     {
         basePosition = transform.position;
         facePosition = transform.GetComponentInParent<NPCController>().facePosition;
 
-        if (facePosition.x * facePosition.y < 0)
-        {
-            range = 180.0f - range;
-        }
+        //if (facePosition.x * facePosition.y < 0)
+        //{
+        //    range = 180.0f - range;
+        //}
 
         if (lineNum < lineRenderers.Count)
         {
@@ -54,10 +55,69 @@ public class VisualRange : MonoBehaviour
                 lineObj.layer = 5;
                 lineObj.transform.SetParent(transform);
                 LineRenderer line = lineObj.AddComponent<LineRenderer>();
+                line.sortingLayerName = "UI";
 
                 setLine(line, count);
 
                 lineRenderers.Add(line);
+            }
+        }
+
+        // edges
+        if (lineNum - 1 < edges.Count)
+        {
+            int count = 0;
+            for (; count < lineNum - 1; count++)
+            {
+                LineRenderer line = edges[count];
+
+                line.positionCount = 2;
+                line.SetPosition(0, lineRenderers[count].GetPosition(1));
+                line.SetPosition(1, lineRenderers[count + 1].GetPosition(1));
+                line.startColor = Color.blue;
+                line.endColor = Color.blue;
+                line.startWidth = 0.05f;
+                line.endWidth = 0.05f;
+            }
+            for (; count < edges.Count;)
+            {
+                LineRenderer line = edges[count - 1];
+                edges.Remove(line);
+                Destroy(line.gameObject);
+            }
+        }
+        else
+        {
+            int count = 0;
+            for (; count < edges.Count; count++)
+            {
+                LineRenderer line = edges[count];
+
+                line.positionCount = 2;
+                line.SetPosition(0, lineRenderers[count].GetPosition(1));
+                line.SetPosition(1, lineRenderers[count + 1].GetPosition(1));
+                line.startColor = Color.blue;
+                line.endColor = Color.blue;
+                line.startWidth = 0.05f;
+                line.endWidth = 0.05f;
+            }
+            for (; count < lineNum - 1; count++)
+            {
+                GameObject lineObj = new GameObject($"Visial Range Edge {count + 1}");
+                lineObj.layer = 5;
+                lineObj.transform.SetParent(transform);
+                LineRenderer line = lineObj.AddComponent<LineRenderer>();
+
+                line.sortingLayerName = "UI";
+                line.positionCount = 2;
+                line.SetPosition(0, lineRenderers[count].GetPosition(1));
+                line.SetPosition(1, lineRenderers[count + 1].GetPosition(1));
+                line.startColor = Color.blue;
+                line.endColor = Color.blue;
+                line.startWidth = 0.05f;
+                line.endWidth = 0.05f;
+
+                edges.Add(line);
             }
         }
     }
@@ -66,9 +126,23 @@ public class VisualRange : MonoBehaviour
     {
         line.positionCount = 2;
         line.SetPosition(0, basePosition);
-        line.SetPosition(1, basePosition + (new Vector2(1, Mathf.Tan((range / 2 - range / (lineNum - 1) * n) / 180.0f * Mathf.PI)) * facePosition).normalized * distance);
+        if (range == 180 && (n == 0 || n == lineNum - 1))
+        {
+            if (n == 0)
+            {
+                line.SetPosition(1, basePosition + new Vector2(0, distance));
+            }
+            else if (n == lineNum - 1)
+            {
+                line.SetPosition(1, basePosition - new Vector2(0, distance));
+            }
+        }
+        else
+        {
+            line.SetPosition(1, basePosition + (new Vector2(1, Mathf.Tan((range / 2 - range / (lineNum - 1) * n) / 180.0f * Mathf.PI)) * facePosition).normalized * distance);
+        }
         line.startColor = Color.blue;
-        line.endColor = Color.red;
+        line.endColor = Color.blue;
         line.startWidth = 0.05f;
         line.endWidth = 0.05f;
     }
