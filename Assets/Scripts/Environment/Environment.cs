c83dc1602879eb5ef083f7f0b128f968264c1cf5
ASLEXF <getsheng6456@gmail.com>
2026-04-04 01:48:48 -0400

    Add projectile control in Environment.cs

--- Assets/Scripts/Environment/Environment.cs
+++ Assets/Scripts/Environment/Environment.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Environment : Singleton<Environment>
 {
+    GameObject projectiles;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        projectiles = new GameObject("Projectiles");
+        projectiles.transform.SetParent(gameObject.transform);
+    }
+
+    public void AddProjectile(GameObject projectile)
+    {
+        projectile.transform.SetParent(projectiles.transform);
+    }
 }
